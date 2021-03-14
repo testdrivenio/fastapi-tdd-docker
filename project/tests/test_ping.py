8f323575f5dbfc65870f64011fb5f563e9fc0737
Michael Herman <hermanmu@gmail.com>
2021-03-14 11:07:30 -0500

    add multistage docker build

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -4,4 +4,4 @@
 def test_ping(test_app):
     response = test_app.get("/ping")
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
+    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
