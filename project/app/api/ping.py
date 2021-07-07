0274549a2f86c1eb5a24d8d8bcb2ec7be254050f
Michael Herman <hermanmu@gmail.com>
2021-07-07 10:12:50 -0500

    testing ci/cd

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -11,7 +11,7 @@ router = APIRouter()
 @router.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
-        "ping": "pong!",
+        "ping": "pong",
         "environment": settings.environment,
         "testing": settings.testing,
     }
