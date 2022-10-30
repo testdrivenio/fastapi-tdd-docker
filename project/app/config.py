839bcdf53b8ab32346b8d10621b4e24dcc696796
Michael Herman <hermanmu@gmail.com>
2022-10-30 10:48:15 -0500

    updates

--- project/app/config.py
+++ project/app/config.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from functools import lru_cache
 
 from pydantic import AnyUrl, BaseSettings
@@ -8,9 +7,9 @@ log = logging.getLogger("uvicorn")
 
 
 class Settings(BaseSettings):
-    environment: str = os.getenv("ENVIRONMENT", "dev")
-    testing: bool = os.getenv("TESTING", 0)
-    database_url: AnyUrl = os.environ.get("DATABASE_URL")
+    environment: str = "dev"
+    testing: bool = 0
+    database_url: AnyUrl = None
 
 
 @lru_cache()
